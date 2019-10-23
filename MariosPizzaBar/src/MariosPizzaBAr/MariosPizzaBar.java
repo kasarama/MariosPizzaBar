9bb05d2daf70d311e46d874dd067cd87f21fbbcc
Matti Hansen <cph-mh829@cphbusiness.dk>
2019-10-23 14:55:08 +0200

    Made some of the meny class

--- MariosPizzaBar/src/MariosPizzaBAr/MariosPizzaBar.java
+++ MariosPizzaBar/src/MariosPizzaBAr/MariosPizzaBar.java
@@ -5,6 +5,10 @@
  */
 package MariosPizzaBAr;
 
+import java.io.FileNotFoundException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Magdalena
@@ -16,6 +20,13 @@ public class MariosPizzaBar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Menu x = new Menu();
+        try {
+            x.readFiles();
+        } catch (FileNotFoundException ex) {
+            //Logger.getLogger(MariosPizzaBar.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error");
+        }
     }
     
 }
--- MariosPizzaBar/src/MariosPizzaBAr/Menu.java
+++ MariosPizzaBar/src/MariosPizzaBAr/Menu.java
@@ -1,14 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package MariosPizzaBAr;
 
-/**
- *
- * @author Magdalena
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.FileReader;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Menu {
+        
+        /*int nr;
+        String name;
+        String ingredientser;
+        int pris;*/
+    public Menu() {
+        
+        /*this.nr = get;
+        this.name = name;
+        this.ingredientser = ingredientser;
+        this.pris = pris;*/
+    }
     
+    public void readFiles() throws FileNotFoundException{
+     String filename= "Data/Pizzaer.txt";
+                File textfile = new File(filename); 
+                Scanner in = new Scanner(textfile);
+                  
+                while(in.hasNextLine()){  
+                    String line = in.nextLine();
+                    System.out.println(line);
+                    
+                }
+                in.close();   
+    }
+    public void makePizza(){
+        String filename= "Data/Pizzaer.txt";
+        File textfile = new File(filename); 
+        String line ="";
+        try {
+            Scanner in = new Scanner(textfile);
+            while (in.hasNextLine()){
+                line= in.nextLine();
+                String[] myArr=line.split(";");
+                Pizza tmpPizza= new Pizza(myArr[0], myArr[1], myArr[2], myArr[3]);
+                pizzaCompound.addPizza(tmpPizza);  
+            }
+        } catch (FileNotFoundException ex) {
+            System.out.println("Can't find file");;
+        }
+    }
+    
+	
 }
