f0594f3fce0277e3d18c5bce13a0295ee3fb0f59
kasarama <cph-mw216@cphbusiness.dk>
2019-10-23 12:56:26 +0200

    Projects body

--- MariosPizzaBar/src/MariosPizzaBAr/MariosPizzaBar.java
+++ MariosPizzaBar/src/MariosPizzaBAr/MariosPizzaBar.java
@@ -3,13 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication50;
+package MariosPizzaBAr;
 
 /**
  *
  * @author Magdalena
  */
-public class JavaApplication50 {
+public class MariosPizzaBar {
 
     /**
      * @param args the command line arguments
