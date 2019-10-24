a4f97559fa4343524b0406d4f9dffa163876178c
kasarama <cph-mw216@cphbusiness.dk>
2019-10-24 14:57:54 +0200

    order konstruktør

--- MariosPizzaBar/src/MariosPizzaBAr/Ordrer.java
+++ MariosPizzaBar/src/MariosPizzaBAr/Ordrer.java
@@ -1,43 +1,41 @@
-
 package MariosPizzaBar;
+
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.Calendar;
 
-
 public class Ordrer {
 
-    private static int nummerMax =0;
+    private static int nummerMax = 0;
     private int nummer;
     private int samletPris;
     private Date afTid;
     private ArrayList<Pizza> ordrer = new ArrayList();
-    
 
     public void tilføjPizza(Pizza pizza) {
         ordrer.add(pizza);
     }
-    
-    public double samletPris(){
-        double prisSum=0;
-        
-        for(Pizza pizza: ordrer){
-            prisSum=prisSum+pizza.getPizzaPrice();
+
+    public double samletPris() {
+        double prisSum = 0;
+
+        for (Pizza pizza : ordrer) {
+            prisSum = prisSum + pizza.getPizzaPrice();
         }
-                return prisSum;
+        return prisSum;
     }
-    
-    public void pickUpTime(){
+
+    public void pickUpTime() {
         Calendar calendar = Calendar.getInstance();
 
         int hour = calendar.get(Calendar.HOUR);
-        int minute = calendar.get(Calendar.MINUTE)+30;
+        int minute = calendar.get(Calendar.MINUTE) + 30;
         int second = calendar.get(Calendar.SECOND);
-        System.out.println(hour+":"+minute+":"+second);
-        
+        System.out.println(hour + ":" + minute + ":" + second);
 
-    
+    }
+
     public Ordrer() {
         this.nummer = nummerMax++;
         this.ordrer = ordrer;
@@ -50,12 +48,12 @@ public class Ordrer {
 
     @Override
     public String toString() {
-        String result="";
-        for(Pizza pizza: ordrer){
-            result+=pizza.toString();
+        String result = "";
+        for (Pizza pizza : ordrer) {
+            result += pizza.toString();
         }
         return "Ordrer " + nummer + ":" + "" + "\n"
-                + result+ "SamletPris: " + samletPris + ", afTid=" + afTid + ", ordrer=" + ordrer + '}';
+                + result + "SamletPris: " + samletPris + ", afTid=" + afTid + ", ordrer=" + ordrer + '}';
     }
 
     public static int getNummerMax() {
@@ -78,7 +76,4 @@ public class Ordrer {
         return ordrer;
     }
 
-    
-
-    
 }
