29d0456e7aacc6067051f0e4ba3d95ce90020e2f
kasarama <cph-mw216@cphbusiness.dk>
2019-10-24 14:52:07 +0200

    Order classe rattet

--- MariosPizzaBar/src/MariosPizzaBAr/Ordrer.java
+++ MariosPizzaBar/src/MariosPizzaBAr/Ordrer.java
@@ -1,32 +1,73 @@
 
 package MariosPizzaBar;
-
+import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.Calendar;
 
-/**
- *
- * @author Magdalena
- */
+
 public class Ordrer {
 
+    private static int nummerMax =0;
+    private int nummer;
+    private int samletPris;
     private Date afTid;
-    ArrayList<Pizza> ordrer;
+    private ArrayList<Pizza> ordrer = new ArrayList();
+    
 
     public void tilføjPizza(Pizza pizza) {
         ordrer.add(pizza);
     }
+    
+    public double samletPris(){
+        double prisSum=0;
+        
+        for(Pizza pizza: ordrer){
+            prisSum=prisSum+pizza.getPizzaPrice();
+        }
+                return prisSum;
+    }
+    
+    public void pickUpTime(){
+        Calendar calendar = Calendar.getInstance();
 
-   
-    public Ordrer(ArrayList<Pizza> ordrer) {
+        int hour = calendar.get(Calendar.HOUR);
+        int minute = calendar.get(Calendar.MINUTE)+30;
+        int second = calendar.get(Calendar.SECOND);
+        System.out.println(hour+":"+minute+":"+second);
+        
+
+    
+    public Ordrer() {
+        this.nummer = nummerMax++;
         this.ordrer = ordrer;
         afTid = new Date();
+        this.samletPris = ordrer.SamletPris();
+        //Date date = new Date();
+        //System.out.println(new Timestamp(date.getTime()));
 
     }
 
     @Override
     public String toString() {
-        return "Ordrer{" + "afTid=" + afTid + ", ordrer=" + ordrer + '}';
+        String result="";
+        for(Pizza pizza: ordrer){
+            result+=pizza.toString();
+        }
+        return "Ordrer " + nummer + ":" + "" + "\n"
+                + result+ "SamletPris: " + samletPris + ", afTid=" + afTid + ", ordrer=" + ordrer + '}';
+    }
+
+    public static int getNummerMax() {
+        return nummerMax;
+    }
+
+    public int getNummer() {
+        return nummer;
+    }
+
+    public int getSamletPris() {
+        return samletPris;
     }
 
     public Date getAfTid() {
@@ -37,4 +78,7 @@ public class Ordrer {
         return ordrer;
     }
 
+    
+
+    
 }
