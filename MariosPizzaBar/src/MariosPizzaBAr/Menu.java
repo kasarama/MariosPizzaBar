cd76303791ea7db71aa469e41e8f51b3b4b6c3c9
kasarama <cph-mw216@cphbusiness.dk>
2019-10-24 15:41:09 +0200

    work on pizza and order

--- MariosPizzaBar/src/MariosPizzaBAr/Menu.java
+++ MariosPizzaBar/src/MariosPizzaBAr/Menu.java
@@ -45,7 +45,7 @@ public class Menu {
                 line= in.nextLine();
                 String[] myArr=line.split(";");
                 Pizza tmpPizza= new Pizza(Integer.parseInt(myArr[0]), myArr[1], myArr[2], Integer.parseInt(myArr[3]));
-                order.addPizza(tmpPizza);  
+               // order.addPizza(tmpPizza);  
             }
         } catch (FileNotFoundException ex) {
             System.out.println("Can't find file");;
