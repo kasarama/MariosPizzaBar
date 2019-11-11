176ab2d15df26f874931c23de9151777a6b9f22e
LouYourLife <vibekenordestgaard@gmail.com>
2019-11-11 10:49:34 +0100

    Nothing

--- MariosPizzaBar/src/MariosPizzaBAr/DBConnector.java
+++ MariosPizzaBar/src/MariosPizzaBAr/DBConnector.java
@@ -2,18 +2,24 @@ package MariosPizzaBAr;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnector {
-    public static Connection getConnection(){
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
 
         Connection connection = null;
-
-        String url = "jdbc:mysql://localhost:3306/pizza?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false";
+        
+        String url = "jdbc:mysql://localhost:3306/pizza?";
+        url += "serverTimezone=UTC&allowPublicKeyRetrieval=true&";
+        url += "useSSL=false";
         String user = "root";
-        String kodeord = "Matti12345h";
+        String password = "AtM12K13BoC185";
+        
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(url,user,password);
 
         try {
-            connection = DriverManager.getConnection(url,user,kodeord);
+            connection = DriverManager.getConnection(url,user,password);
         } catch (Exception e){
             e.printStackTrace();
         }
