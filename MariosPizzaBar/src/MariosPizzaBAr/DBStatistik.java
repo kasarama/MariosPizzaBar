8764edb49c7cea6055f38dc47236c0e35497f3c5
MattiBSH <cph-mh829@cphbusiness.dk>
2019-11-12 20:28:19 +0100

    prøvede men du ved

--- MariosPizzaBar/src/MariosPizzaBAr/DBStatistik.java
+++ MariosPizzaBar/src/MariosPizzaBAr/DBStatistik.java
@@ -9,8 +9,7 @@ import java.sql.Statement;
 
 public class DBStatistik {
 public void LæsStatistikSnitPris() throws SQLException, ClassNotFoundException {
-
-                Connection connection = null;
+        Connection connection = null;
         Statement statement = null;
         //MainController.mainControllerLooop();
         connection = DBConnector.getConnection();
@@ -18,11 +17,25 @@ public void LæsStatistikSnitPris() throws SQLException, ClassNotFoundException 
         statement = connection.createStatement();
         statement.execute(q);
         ResultSet resultSet = statement.executeQuery("SELECT AVG(sum) FROM pizza.ordrer;");
-
         ResultSetMetaData rsmd = resultSet.getMetaData();
-        int columnsNumber = rsmd.getColumnCount();
         while (resultSet.next()) {
-            
+                String columnValue = resultSet.getString(1);
+                System.out.print(columnValue + " " + rsmd.getColumnName(1));
+            }
+            System.out.println("");
+        }
+public void LæsStatistikDagSnitPris(String dato) throws SQLException, ClassNotFoundException {
+        Connection connection = null;
+        Statement statement = null;
+        //MainController.mainControllerLooop();
+        connection = DBConnector.getConnection();
+        dato=null;
+        String q = "SELECT AVG(sum) FROM pizza.ordrer where Dato=\"2019.10.12\";";
+        statement = connection.createStatement();
+        statement.execute(q);
+        ResultSet resultSet = statement.executeQuery("SELECT AVG(sum) FROM pizza.ordrer where dato?;");
+        ResultSetMetaData rsmd = resultSet.getMetaData();
+        while (resultSet.next()) {
                 String columnValue = resultSet.getString(1);
                 System.out.print(columnValue + " " + rsmd.getColumnName(1));
             }
--- MariosPizzaBar/src/MariosPizzaBAr/MariosPizzaBar.java
+++ MariosPizzaBar/src/MariosPizzaBAr/MariosPizzaBar.java
@@ -12,6 +12,6 @@ public class MariosPizzaBar {
 
     public static void main(String[] args) throws ClassNotFoundException, SQLException {
         DBStatistik db=new DBStatistik();
-        db.LæsStatistik();
+        db.LæsStatistikSnitPris();
     }
 }
