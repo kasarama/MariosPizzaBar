d02c47bb7771b1ab1a493c95828fde715adf701c
LouYourLife <vibekenordestgaard@gmail.com>
2019-11-13 15:36:22 +0100

    No visible changes

--- MariosPizzaBar/test/MariosPizzaBAr/OrderFactoryTest.java
+++ MariosPizzaBar/test/MariosPizzaBAr/OrderFactoryTest.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class OrderFactoryTest {
-    PizzaFactory pf = new PizzaFactory();
-    Pizza pizza1 = pf.getPizzaByPosition(1);
-    Pizza pizza2 = pf.getPizzaByPosition(1);
-    OrderFactory o = new OrderFactory();
 
     @Test
-        public void testSum() throws ClassNotFoundException, SQLException {
+    public void testSum() throws ClassNotFoundException, SQLException {
+        PizzaFactory pf = new PizzaFactory();
+        Pizza pizza1 = pf.getPizzaByPosition(1);
+        Pizza pizza2 = pf.getPizzaByPosition(1);
+        OrderFactory o = new OrderFactory();
         Order or = o.orderMaker();
         or.addPizza(pizza1);
         or.addPizza(pizza2);
